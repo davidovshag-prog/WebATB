947559d78f717765bc92589cb90b0e1bed55476c
Roman <davidovshag@gmail.com>
2026-01-28 11:43:33 +0200

    Add Model Create

--- WebATB/Controllers/MainController.cs
+++ WebATB/Controllers/MainController.cs
@@ -16,35 +16,37 @@ public class MainController : Controller
             Phone = "+38(098)235 6421",
             Image = "1.jpg"
         });
-
         model.Add(new UserItemModel
         {
             Id = 2,
             Name = "Рижий Олексій Мохнович",
-            Phone = "+38(098)224 6165",
-            Image = "2.jpg"
+            Phone = "+38(098)230 6421",
+            Image = "2.webp"
         });
-
         model.Add(new UserItemModel
         {
             Id = 3,
             Name = "Галоша Іванна Ігорівна",
-            Phone = "+38(098)378 4756",
+            Phone = "+38(098)236 6421",
             Image = "3.jpg"
         });
 
         return View(model);
     }
 
+    [HttpGet] //метод для відображення сторінки створення нового користувача
     public IActionResult Create()
     {
         return View();
     }
-
     [HttpPost]
-    public IActionResult Create(UserItemModel model, IFormFile imageFile)
+    public IActionResult Create(UserCreateModel model)
     {
-        return RedirectToAction("Index");
+        if(!ModelState.IsValid)
+        {
+            return View(model);
+        } // Якщо модель валідна то дані будемо зберігати в 
+          // список і переходити на іншу сторінку
+        return RedirectToAction(nameof(Index));
     }
-
 }
---
+++ WebATB/Models/Users/UserCreateModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebATB.Models.Users;
+
+public class UserCreateModel
+{
+    [Display(Name = "Прізвище")]
+    [Required(ErrorMessage = "Вкажіть прізвище")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Display(Name = "Ім'я")]
+    [Required(ErrorMessage = "Вкажіть ім'я")]
+    public string Name { get; set; } = string.Empty;
+
+}
